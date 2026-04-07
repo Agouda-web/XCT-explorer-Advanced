1f2528300d16e47127ff88b5ac75a420ca126c6e
Agouda-web <praicyann189@gmail.com>
2026-04-07 11:09:43 +0200

    Update xct_explorer_expert.py

--- xct_explorer_expert.py
+++ xct_explorer_expert.py
@@ -294,14 +294,14 @@ with tabComposition:
 ############################ Display plots ################################
 st.divider()
 col5,col4=st.columns(2,gap='large')
-   with col4:
+with col4:
        st.subheader('Total transmission',
                   help='Percent of x-rays that penetrate through the :blue[Filter (light blue)], the :green[Sample (green)] and the :orange[Sample + Filter (orange)] at various energies')
         dfTotalTransm4Plot2, energyAt10percTransm = transmission()
         st.write(':green[Sample]  -  :blue[Filter]  -  :orange[Sample+Filter]')
         with st.expander('Transmission Table'):
             st.table(dfTotalTransm4Plot2)    
-    with col5:
+with col5:
         st.subheader('Attenuation',
                   help='Energies with large difference between curves give better contrast. Note: if the curves are matching the phases will have similar greyvalues in the final image)')
        # attenuation_energy()
